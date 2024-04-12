f090a57775cb4dceaab7914eebcabb13361186b4
xRoyalPhantom <s355huan@uwaterloo.ca>
2024-04-12 10:00:37 -0700

    data\

---
+++ round1/lr_starfruit_38_mm_ame - Copy.py
@@ -0,0 +1,306 @@
+import json
+import jsonpickle
+from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
+from typing import Any, OrderedDict
+import math
+import numpy as np
+
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+        self.max_log_length = 3750
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]], conversions: int, trader_data: str) -> None:
+        base_length = len(self.to_json([
+            self.compress_state(state, ""),
+            self.compress_orders(orders),
+            conversions,
+            "",
+            "",
+        ]))
+
+        # We truncate state.traderData, trader_data, and self.logs to the same max. length to fit the log limit
+        max_item_length = (self.max_log_length - base_length) // 3
+
+        print(self.to_json([
+            self.compress_state(state, self.truncate(state.traderData, max_item_length)),
+            self.compress_orders(orders),
+            conversions,
+            self.truncate(trader_data, max_item_length),
+            self.truncate(self.logs, max_item_length),
+        ]))
+
+        self.logs = ""
+
+    def compress_state(self, state: TradingState, trader_data: str) -> list[Any]:
+        return [
+            state.timestamp,
+            trader_data,
+            self.compress_listings(state.listings),
+            self.compress_order_depths(state.order_depths),
+            self.compress_trades(state.own_trades),
+            self.compress_trades(state.market_trades),
+            state.position,
+            self.compress_observations(state.observations),
+        ]
+
+    def compress_listings(self, listings: dict[Symbol, Listing]) -> list[list[Any]]:
+        compressed = []
+        for listing in listings.values():
+            compressed.append([listing["symbol"], listing["product"], listing["denomination"]])
+
+        return compressed
+
+    def compress_order_depths(self, order_depths: dict[Symbol, OrderDepth]) -> dict[Symbol, list[Any]]:
+        compressed = {}
+        for symbol, order_depth in order_depths.items():
+            compressed[symbol] = [order_depth.buy_orders, order_depth.sell_orders]
+
+        return compressed
+
+    def compress_trades(self, trades: dict[Symbol, list[Trade]]) -> list[list[Any]]:
+        compressed = []
+        for arr in trades.values():
+            for trade in arr:
+                compressed.append([
+                    trade.symbol,
+                    trade.price,
+                    trade.quantity,
+                    trade.buyer,
+                    trade.seller,
+                    trade.timestamp,
+                ])
+
+        return compressed
+
+    def compress_observations(self, observations: Observation) -> list[Any]:
+        conversion_observations = {}
+        for product, observation in observations.conversionObservations.items():
+            conversion_observations[product] = [
+                observation.bidPrice,
+                observation.askPrice,
+                observation.transportFees,
+                observation.exportTariff,
+                observation.importTariff,
+                observation.sunlight,
+                observation.humidity,
+            ]
+
+        return [observations.plainValueObservations, conversion_observations]
+
+    def compress_orders(self, orders: dict[Symbol, list[Order]]) -> list[list[Any]]:
+        compressed = []
+        for arr in orders.values():
+            for order in arr:
+                compressed.append([order.symbol, order.price, order.quantity])
+
+        return compressed
+
+    def to_json(self, value: Any) -> str:
+        return json.dumps(value, cls=ProsperityEncoder, separators=(",", ":"))
+
+    def truncate(self, value: str, max_length: int) -> str:
+        if len(value) <= max_length:
+            return value
+
+        return value[:max_length - 3] + "..."
+
+logger = Logger()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# OUR CODE -------------------------------- -------------------------------- -------------------------------- -------------------------------- --------------------------------
+
+class HistoricalVWAP:
+    def __init__(self, bv=0, sv=0, bpv=0, spv=0):
+        self.buy_volume = bv
+        self.sell_volume = sv
+        self.buy_price_volume = bpv
+        self.sell_price_volume = spv
+
+class RecordedData: 
+    def __init__(self):
+        self.amethyst_hvwap = HistoricalVWAP()
+        self.starfruit_cache = []
+        self.LIMIT = {'AMETHYSTS' : 20, 'STARFRUIT' : 20}
+        self.INF = int(1e9)
+        self.STARFRUIT_CACHE_SIZE = 35
+        self.AME_RANGE = 2
+        self.POSITION = {'AMETHYSTS' : 0, 'STARFRUIT' : 0}
+
+
+class Trader:
+    LIMIT = {'AMETHYSTS' : 20, 'STARFRUIT' : 20}
+    INF = int(1e9)
+    STARFRUIT_CACHE_SIZE = 38
+    AME_RANGE = 2
+    POSITION = {}
+
+    def estimate_starfruit_price(self, cache):
+        x = np.array([i for i in range(self.STARFRUIT_CACHE_SIZE)])
+        y = np.array(cache)
+        A = np.vstack([x, np.ones(len(x))]).T
+        m, c = np.linalg.lstsq(A, y, rcond=None)[0]
+        return int(round(self.STARFRUIT_CACHE_SIZE * m + c))
+
+        # coef = [-0.01869561,  0.0455032 ,  0.16316049,  0.8090892] # 
+        # intercept = 4.481696494462085
+        # estimated_price = intercept
+        # for i, val in enumerate(cache):
+        #     estimated_price += val * coef[i]
+
+        # return int(round(estimated_price))
+
+
+    # gets the total traded volume of each time stamp and best price
+    # best price in buy_orders is the max; best price in sell_orders is the min
+    # buy_order indicates orders are buy or sell orders
+    def get_volume_and_best_price(self, orders, buy_order):
+        volume = 0
+        best = 0 if buy_order else self.INF
+
+        for price, vol in orders.items():
+            if buy_order:
+                volume += vol
+                best = max(best, price)
+            else:
+                volume -= vol
+                best = min(best, price)
+
+        return volume, best
+    
+
+    # given estimated bid and ask prices, market take if there are good offers, otherwise market make 
+    # by pennying or placing our bid/ask, whichever is more profitable
+    def calculate_orders(self, product, order_depth, our_bid, our_ask):
+        orders: list[Order] = []
+        
+        sell_orders = OrderedDict(sorted(order_depth.sell_orders.items()))
+        buy_orders = OrderedDict(sorted(order_depth.buy_orders.items(), reverse=True))
+
+        sell_vol, best_sell_price = self.get_volume_and_best_price(sell_orders, buy_order=False)
+        buy_vol, best_buy_price = self.get_volume_and_best_price(buy_orders, buy_order=True)
+
+        logger.print(f'Product: {product} - best sell: {best_sell_price}, best buy: {best_buy_price}')
+
+        position = self.POSITION[product]
+        limit = self.LIMIT[product]
+
+        # penny the current highest bid / lowest ask 
+        penny_buy = best_buy_price+1
+        penny_sell = best_sell_price-1
+
+        bid_price = min(penny_buy, our_bid)
+        ask_price = max(penny_sell, our_ask)
+
+        # MARKET TAKE ASKS (buy items)
+        for ask, vol in sell_orders.items():
+            if position < limit and (ask <= our_bid or (position < 0 and ask == our_bid+1)): 
+                num_orders = min(-vol, limit - position)
+                position += num_orders
+                orders.append(Order(product, ask, num_orders))
+
+        # MARKET MAKE BY PENNYING
+        if position < limit:
+            num_orders = limit - position
+            orders.append(Order(product, bid_price, num_orders))
+            position += num_orders
+
+        position = self.POSITION[product] # RESET POSITION
+
+        # MARKET TAKE BIDS (sell items)
+        for bid, vol in buy_orders.items():
+            if position > -limit and (bid >= our_ask or (position > 0 and bid+1 == our_ask)):
+                num_orders = max(-vol, -limit-position)
+                position += num_orders
+                orders.append(Order(product, bid, num_orders))
+
+        # MARKET MAKE BY PENNYING
+        if position > -limit:
+            num_orders = -limit - position
+            orders.append(Order(product, ask_price, num_orders))
+            position += num_orders 
+
+        return orders
+
+
+                      
+    def calculate_vwap(self, orders):
+        total_volume = sum(amount for _, amount in orders.items())
+        total_price_volume = sum(price * amount for price, amount in orders.items())
+        if total_volume == 0:
+            return 0
+        return total_price_volume / total_volume
+    
+    def run(self, state: TradingState) -> tuple[dict[Symbol, list[Order]], int, str]:
+        result = {}
+        conversions = 0
+
+        if state.traderData == '': # first run, set up data
+            data = RecordedData()
+        else:
+            data = jsonpickle.decode(state.traderData)
+
+        self.LIMIT = data.LIMIT
+        self.INF = data.INF
+        self.STARFRUIT_CACHE_SIZE = data.STARFRUIT_CACHE_SIZE
+        self.AME_RANGE = data.AME_RANGE
+        self.POSITION = data.POSITION
+
+        # update our position 
+        for product in state.order_depths:
+            self.POSITION[product] = state.position[product] if product in state.position else 0
+
+
+        for product in state.order_depths:
+            order_depth: OrderDepth = state.order_depths[product]
+            orders: list[Order] = []
+            
+            if product == "AMETHYSTS":
+                orders += self.calculate_orders(product, order_depth, 10000-self.AME_RANGE, 10000+self.AME_RANGE)
+            
+            elif product == "STARFRUIT":
+                # keep the length of starfruit cache as STARFRUIT_CACHE_SIZE
+                if len(data.starfruit_cache) == self.STARFRUIT_CACHE_SIZE:
+                    data.starfruit_cache.pop(0)
+
+                _, best_sell_price = self.get_volume_and_best_price(order_depth.sell_orders, buy_order=False)
+                _, best_buy_price = self.get_volume_and_best_price(order_depth.buy_orders, buy_order=True)
+
+                data.starfruit_cache.append((best_sell_price+best_buy_price)/2)
+
+                # if cache size is maxed, calculate next price and place orders
+                lower_bound = -self.INF
+                upper_bound = self.INF
+
+                if len(data.starfruit_cache) == self.STARFRUIT_CACHE_SIZE:
+                    lower_bound = self.estimate_starfruit_price(data.starfruit_cache)-2
+                    upper_bound = self.estimate_starfruit_price(data.starfruit_cache)+2
+
+                orders += self.calculate_orders(product, order_depth, lower_bound, upper_bound)
+
+            logger.print(f'placed orders: {orders}')
+
+            # update orders for current product
+            result[product] = orders
+
+        traderData = jsonpickle.encode(data)
+
+        logger.flush(state, result, conversions, traderData)
+        return result, conversions, traderData
